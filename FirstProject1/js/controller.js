6a64738b870b27862fc8f4ed3c7f75cad1b0806b
DimaSavosin <143866241+DimaSavosin@users.noreply.github.com>
2024-12-29 20:13:09 +0300

    controller.js
    обновленный контроллер

--- FirstProject1/js/controller.js
+++ FirstProject1/js/controller.js
@@ -2,6 +2,8 @@ import model from './model.js';
 
 const controller = {
     initialize: function () {
+        model.initializeState();
+
         model.fetchDishes()
             .then(() => {
                 model.state.filteredDishes = model.state.dishes;
@@ -10,20 +12,37 @@ const controller = {
     },
 
     onSelectChange: function (category) {
-        model.filterDishes(category);
+        if (category === '' || category === 'all') {
+            model.state.filteredDishes = model.state.dishes;
+        } else {
+            model.state.filteredDishes = model.state.dishes.filter(dish => dish.category === category);
+        }
     },
 
-
     addToCart: function (dish) {
-        model.addToCart(dish);
+        const existingDish = model.state.cart.find(item => item.id === dish.id);
+        if (existingDish) {
+            existingDish.quantity += 1;
+        } else {
+            model.state.cart.push({ ...dish, quantity: 1 });
+        }
+        localStorage.setItem('cart', JSON.stringify(model.state.cart));
     },
 
     updateCartItem: function (id, change) {
-        model.updateCartItem(id, change);
+        const dishIndex = model.state.cart.findIndex(item => item.id === id);
+        if (dishIndex > -1) {
+            model.state.cart[dishIndex].quantity += change;
+            if (model.state.cart[dishIndex].quantity <= 0) {
+                model.state.cart.splice(dishIndex, 1);
+            }
+        }
+        localStorage.setItem('cart', JSON.stringify(model.state.cart));
     },
 
     clearCart: function () {
-        model.clearCart();
+        model.state.cart = [];
+        localStorage.setItem('cart', JSON.stringify(model.state.cart));
     },
 
     completeOrder: function () {
@@ -32,12 +51,14 @@ const controller = {
             items: [...model.state.cart],
             totalPrice: model.state.cart.reduce((acc, dish) => acc + dish.price * dish.quantity, 0)
         };
-        model.addOrderToHistory(order);
-        model.clearCart();
+        model.state.orderHistory.push(order);
+        localStorage.setItem('orderHistory', JSON.stringify(model.state.orderHistory));
+        this.clearCart();
     },
 
     clearOrderHistory: function () {
         model.state.orderHistory = [];
+        localStorage.setItem('orderHistory', JSON.stringify(model.state.orderHistory));
     }
 };
 
