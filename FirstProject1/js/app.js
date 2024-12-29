eb542af7798b60c4261e222b346bc79144681567
DimaSavosin <143866241+DimaSavosin@users.noreply.github.com>
2024-12-29 20:15:12 +0300

    app.js

--- FirstProject1/js/app.js
+++ FirstProject1/js/app.js
@@ -4,19 +4,9 @@ import model from './model.js';
 import view from "./view.js";
 
 const initializeApp = () => {
-
     watchState();
-
-
-    model.state.cart = JSON.parse(localStorage.getItem('cart')) || [];
-    model.state.orderHistory = JSON.parse(localStorage.getItem('orderHistory')) || [];
-
-
     controller.initialize();
-
-
     view.initialize(controller, model.state);
 };
-
 
 initializeApp();
