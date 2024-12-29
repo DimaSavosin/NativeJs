7e5a2fd51bd4554789b90fe88f7e26be940b791a
DimaSavosin <143866241+DimaSavosin@users.noreply.github.com>
2024-12-29 20:10:00 +0300

    model.js
    обновленная модель

--- FirstProject1/js/model.js
+++ FirstProject1/js/model.js
@@ -1,14 +1,12 @@
-
 const model = {
     state: {
         dishes: [],
-        cart: JSON.parse(localStorage.getItem('cart')) || [],
-        orderHistory: JSON.parse(localStorage.getItem('orderHistory')) || [],
+        cart: [],
+        orderHistory: [],
         filteredDishes: []
     },
 
-
-    fetchDishes: async function () {
+    async fetchDishes() {
         try {
             const response = await fetch('http://localhost:8080/dishes');
             const data = await response.json();
@@ -19,8 +17,7 @@ const model = {
         }
     },
 
-
-    filterDishes: function (category) {
+    filterDishes(category) {
         if (category === '' || category === 'all') {
             this.state.filteredDishes = this.state.dishes;
         } else {
@@ -28,39 +25,9 @@ const model = {
         }
     },
 
-
-    addToCart: function (dish) {
-        const existingDish = this.state.cart.find(item => item.id === dish.id);
-        if (existingDish) {
-            existingDish.quantity += 1;
-        } else {
-            this.state.cart.push({ ...dish, quantity: 1 });
-        }
-        localStorage.setItem('cart', JSON.stringify(this.state.cart));
-    },
-
-
-    updateCartItem: function (id, change) {
-        const dishIndex = this.state.cart.findIndex(item => item.id === id);
-        if (dishIndex > -1) {
-            this.state.cart[dishIndex].quantity += change;
-            if (this.state.cart[dishIndex].quantity <= 0) {
-                this.state.cart.splice(dishIndex, 1);
-            }
-        }
-        localStorage.setItem('cart', JSON.stringify(this.state.cart));
-    },
-
-
-    clearCart: function () {
-        this.state.cart = [];
-        localStorage.setItem('cart', JSON.stringify(this.state.cart));
-    },
-
-
-    addOrderToHistory: function (order) {
-        this.state.orderHistory.push(order);
-        localStorage.setItem('orderHistory', JSON.stringify(this.state.orderHistory));
+    initializeState() {
+        this.state.cart = JSON.parse(localStorage.getItem('cart')) || [];
+        this.state.orderHistory = JSON.parse(localStorage.getItem('orderHistory')) || [];
     }
 };
 
